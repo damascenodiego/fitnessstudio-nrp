ab5007bbff514186980456f7a87a8048c1e642c9
Niels van Harten <niels@cvharten.nl>
2021-02-25 13:17:52 +0100

    Allow RuleSet to use Trace Model
    By registering TracePackage

--- de.uni_ko.fitnessstudio/src/de/uni_ko/fitnessstudio/util/EngineFactory.java
+++ de.uni_ko.fitnessstudio/src/de/uni_ko/fitnessstudio/util/EngineFactory.java
@@ -4,12 +4,18 @@ import org.eclipse.emf.henshin.interpreter.Engine;
 import org.eclipse.emf.henshin.interpreter.impl.ChangeImpl;
 import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
 
+import org.eclipse.emf.henshin.trace.TracePackage;
+
 public class EngineFactory {
 
 	public static Engine createEngine() {
 		Engine result = new EngineImpl();
 		result.getOptions().put(Engine.OPTION_DETERMINISTIC, false);
 		ChangeImpl.PRINT_WARNINGS = false;
+
+		// Register the Trace Model package
+		TracePackage.eINSTANCE.getName();
+		
 		return result;
 	}
 
