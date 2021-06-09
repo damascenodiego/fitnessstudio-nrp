c1110271746bcf4729057e32e7e19908f707e1e5
Niels van Harten <niels@cvharten.nl>
2021-06-09 14:01:19 +0200

    Update MinimiseCost.java

--- single-objective/fitnessstudio.instance.nrp/src/fitnessstudio/instance/nrp/fitness/MinimiseCost.java
+++ single-objective/fitnessstudio.instance.nrp/src/fitnessstudio/instance/nrp/fitness/MinimiseCost.java
@@ -4,18 +4,15 @@ import nrp.model.nrp.*;
 
 public class MinimiseCost {
 
-  public double computeFitness(NRP nextRelease) {
-    double selectedArtifactsCost =
-        nextRelease.getSolutions().get(0).getSelectedArtifacts().stream()
-            .mapToDouble(sa -> sa.getCosts().get(0).getAmount())
-            .sum();
+	public double computeFitness(NRP nextRelease) {
+		double selectedArtifactsCost = nextRelease.getSolutions().get(0).getSelectedArtifacts().stream()
+				.mapToDouble(sa -> sa.getCosts().get(0).getAmount())
+				.sum();
 
-    //System.out.println("Calculated selectedArtifacts cost: " + selectedArtifactsCost);
+		return selectedArtifactsCost;
+	}
 
-    return selectedArtifactsCost;
-  }
-
-  public String getName() {
-    return "Minimise Next Release Cost";
-  }
+	public String getName() {
+		return "Minimise Next Release Cost";
+	}
 }
